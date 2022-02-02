11eff1ace4e5faa5be5b68b39aba85b055c7e979
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-02-02 16:56:50 -0600

    updated base class

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,21 @@ This is the base class from which other classes are derived
 """
 
 
+
 class BaseModel():
     """init the class"""
-    def __init__(self):
-        super().__init__()
+    def __init__(self, id):
+        super().__init__(id)
+    
+    def created_at(self):
+        """Assign date time on creation"""
+    
+    def updated_at(self):
+        """Assign date time on update"""
+
+    def save(self):
+        """Saves attributes"""
+        save = save.self
+    
+    def __str__(self):
+        """__str__ string"""
