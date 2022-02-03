478d4f2ac6b3300c486a0f98dbe002eb6ea6d24a
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-02-03 16:51:52 -0600

    Added parse and prompt to console.py

--- console.py
+++ console.py
@@ -4,27 +4,79 @@ This is the console that the user inputs commands into
 """
 
 
-import cmd, sys, os, datetime, uuid, json
+import cmd
+
+from engine import file_storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """Console class basic init"""
     intro = "Welcome to AirBnB!"
-    prompt = "Select something"
+    prompt = "(hbnb)"
     file = None
+    
+    __class_list = {
+        BaseModel.__name__: BaseModel,
+        User.__name__: User,
+        State.__name__: State,
+        City.__name__: City,
+        Place.__name__: Place,
+        Amenity.__name__: Amenity,
+        Review.__name__: Review
+    }
+    __class_funcs = ["all", "count", "show", "destroy", "update"]
 
+
+    @staticmethod
+    def parse(arg):
+        """Convert args into tuples for interpretation"""
+        arg_list = arg.split(id)
+        narg_list = []
+
+        for a in arg_list:
+            if a != '':
+                narg_list.append(a)
+        return narg_list
+
+    @classmethod
+    def create(self):
+
+    @classmethod
+    def show(self):
+
+    @classmethod
+    def destroy(self):
+
+    @classmethod
+    def all(self):
+
+    @classmethod
+    def update(self):
+
+    @classmethod
+    def empty_line(self):
+        cmd.emptyline()
+
+    @classmethod
     def save(self, arg):
         """Save function"""
         self.file = open(arg, 'w')
 
-    def close(self):
+    @classmethod
+    def EOF(self):
+
+    @classmethod
+    def quit(self):
         """Close the Console"""
         if self.file:
             self.file.close()
             self.file = None
-
-def parse(arg):
-    """Convert args into tuples for interpretation"""
-    return tuple(map(int,arg.split()))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
