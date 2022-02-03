0bc29209817c1e3282b70883783fc3f95a599e2f
jfharris <jfharris1983@gmail.com>
2022-02-03 06:40:06 -0600

    Added docstring info

--- models/place.py
+++ models/place.py
@@ -10,6 +10,17 @@ Place class
 class Place(BaseModel):
     """
     Place class
+    city_id: string - empty string: will be the City.id
+    user_id: string - empty string: will be the User.id
+    name: string - empty string
+    description: string - empty string
+    number_rooms: integer - 0
+    number_bathrooms: integer - 0
+    max_guest: integer - 0
+    price_by_night: integer - 0
+    latitude: float - 0.0
+    longitude: float - 0.0
+    amenity_ids: list of string - empty list: will become list of Amenity.id
     """
 
     city_id = ""
