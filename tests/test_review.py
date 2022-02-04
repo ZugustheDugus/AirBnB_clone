9e80bf8c9dc1cd025f71d9feaf178351f9ee3828
jfharris <jfharris1983@gmail.com>
2022-02-04 02:31:37 -0600

    Added test_review and 1 test

---
+++ tests/test_review.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+unittests for Review
+"""
+import unittest
+from models.base_model import BaseModel
+from models.review import Review
+
+
+class TestReviewClass(unittest.TestCase):
+    """
+    unittests for Review
+    """
+
+    def test_class(self):
+        """
+        1 - tests for empty class in fields
+        """
+        obj = Review()
+
+        self.assertEqual(Review.place_id, "")
+        self.assertEqual(Review.user_id, "")
+        self.assertEqual(Review.text, "")
+        self.assertEqual(obj.place_id, "")
+        self.assertEqual(obj.user_id, "")
+        self.assertEqual(obj.text, "")
