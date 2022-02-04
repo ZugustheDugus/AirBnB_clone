d2330a47cf9ce6cf3e920acd3252b23a3a60e967
jfharris <jfharris1983@gmail.com>
2022-02-04 00:55:20 -0600

    MOved files to correct folder

---
+++ models/engine/file_storage.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+File storage section of the program
+"""
+
+from multiprocessing.sharedctypes import Value
+from os.path import exists
+from json import loads, dumps
+import datetime
+
+from engine import file_storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+class FileStorage():
+    """
+    serializes and/or deserializes JSON file
+    Private attrs:
+    __file_path: string - path to the JSON file (ex: file.json)
+    __objects: dictionary - will store all objects by <class name>.id
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        dict of save objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        stores obj in dict above
+        """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
+        self.save()
+
+    def save(self):
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
+        dict_store = {}
+        for key, value in self.__objects.items():
+            dict_store[key] = value.to_dict()
+        with open(self.__file_path, "w") as f:
+            f.write(dumps(dict_store))
+
+    def reload(self):
+        """
+        reload: deserializes the JSON file to __objects JSON file if
+        the JSON file (__file_path) exists;
+        otherwise, do nothing. No exception should be raised)
+        """
+        if exists(self.__file_path) is False:
+            return
+
+        with open(self.__file_path, "r") as f:
+            dicts = loads(f.read())
+
+        from .our_objects import classes
+
+        self.__objects = {}
+        for id in dict in dicts.items():
+            class_name = id.split(".")[0]
+            cls = classes[class_name]
+            self.__objects[id] = cls(**dict)
