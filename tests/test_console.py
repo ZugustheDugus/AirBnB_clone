e6035b9086a141013c336c1b21131bf68daa7d67
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-02-05 10:34:49 -0600

    Adding tests to the console

--- tests/test_console.py
+++ tests/test_console.py
@@ -12,6 +12,15 @@ class TestConsole(unittest.TestCase):
     def test_console(self):
         """"""
         obj = HBNBCommand()
-        self.assertEqual
-        
+        self.assertEqual(HBNBCommand, "")
+        self.assertEqual(HBNBCommand.default, "")
+        self.assertEqual(HBNBCommand.destroy, "")
+        self.assertEqual(HBNBCommand.do_all, "")
+        self.assertEqual(HBNBCommand.do_create, "")
+        self.assertEqual(HBNBCommand.close "")
+        self.assertEqual(HBNBCommand.cmdloop, "")
+        self.assertEqual(HBNBCommand.count, "")
+        self.assertEqual(HBNBCommand.do_EOF, "")
+
         self.assertIsInstance(obj, HBNBCommand)
+        self.assertIsInstance(cls, HBNBCommand)
