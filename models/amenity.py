8abf487d4bc4f114c3d0a6089165d2ec9938df20
jfharris <jfharris1983@gmail.com>
2022-02-06 00:22:19 -0600

    Edited amenity and state to inherit from BaseModel

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,7 @@ This class stores info about the amenities in an area
 """
 from models.base_model import BaseModel
 
-class Amenity():
+class Amenity(BaseModel):
     """
     Init the class
     name: string - empty string
--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ State class, which stores state info
 from models.base_model import BaseModel
 
 
-class State():
+class State(BaseModel):
     """
     init the class
     name: string - empty string
